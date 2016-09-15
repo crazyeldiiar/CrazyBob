d0b895b954f948a31d65161fd4430aa92d1ea6cd
unknown <duishenaliev_ed@12-10.iuca.loc>
2016-09-15 12:06:12 +0600

    Ggg

---
+++ Naimenshee.py
@@ -0,0 +1,7 @@
+num1 = raw_input ("> ")
+num2 = raw_input ("> ")
+if num1 > num2:
+	print "naimenshee chislo" + num2
+else:
+	print "naimenshee chislo" + num2
+
---
+++ test1.py
@@ -0,0 +1,16 @@
+print " in a battle killed Spartacus ?"
+print " 1. in the last "
+print " 2. I dont know "
+riddle1 = raw_input ("> ")
+if riddle1 == "1":
+    print "Good "
+else:
+	print "Stupid "
+print "For over an hour , less than a minute "
+print " 1. second "
+print " 2. Day "
+riddle2 = raw_input ("> ")
+if riddle2 == "2":
+	print "excelent"
+else:
+	print "ERROR"
